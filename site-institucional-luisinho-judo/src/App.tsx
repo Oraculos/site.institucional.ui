2796271645485712590f83b68340672059e2a02a
IgorMoura1 <igor.moura@sptech.school>
2024-09-30 19:23:47 -0300

    feat/ Subindo componentes header, footer, hero, secondary, card, aboutLuis

--- site-institucional-luisinho-judo/src/App.tsx
+++ site-institucional-luisinho-judo/src/App.tsx
@@ -4,6 +4,7 @@ import Header from './componentes/head'
 import Hero from './componentes/hero'
 import SecondarySection from './componentes/secondarySection'
 import Footer from './componentes/footer'
+import AboutLuis from './componentes/aboutLuis'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Header />
     <Hero />
     <SecondarySection />
+    <AboutLuis />
     <Footer />
     </div>
   )
