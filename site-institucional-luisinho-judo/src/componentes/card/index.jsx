90ed9d726011f807093c1db64d929804967c3367
Igor Moura <igor.sousamoura@outlook.com>
2024-10-07 21:07:11 -0300

    feat/ seção dos professores terminada, iniciada a seção de como participar do projeto

--- site-institucional-luisinho-judo/src/componentes/card/index.jsx
+++ site-institucional-luisinho-judo/src/componentes/card/index.jsx
@@ -1,10 +1,12 @@
 import "./styles.css";
+import TeacherImage from "../card/assets/teacher.jpg";
+
 export default function Card() {
     return (
         <div className="card">
             <div className="card-content">
                 <div className="card-photo">
-                    {/* <img src={ImageTeacher} alt="" /> */}
+                    <img src={TeacherImage} alt="" />
                 </div>
                 <p className="card-text">
                     This is the content of the card. You can add more text here.
--- site-institucional-luisinho-judo/src/componentes/teachers/index.jsx
+++ site-institucional-luisinho-judo/src/componentes/teachers/index.jsx
@@ -1,11 +1,9 @@
 import './styles.css';
 import Card from '../card';
-import BackgroundImage from '../teachers/assets/background.svg';
 
 export default function Teachers() {
     return (
         <div className="backgroundTeachers">
-            <img src={BackgroundImage} alt="" />
             <div className="contentTeacher">
                 <h1>PROFESSORES</h1>
                 <div className="cardContainer">
